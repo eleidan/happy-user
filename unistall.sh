884f2a84bd0201f7dae382ba5a0b647d9ef92545
John Doe <johndoe@example.com>
2017-08-16 17:41:50 +0300

    Disable apport in unistall script

--- unistall.sh
+++ unistall.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
-sudo apt-get purge \
-  vim-tiny \
-  thunderbird
+TODO_LIST="vim-tiny thunderbird"
+echo "Uninstall:\n\t$TODO_LIST"
+sudo apt-get purge $TODO_LIST
+
+echo "Disable apport reporting"
+sudo sed -i -e 's/enabled=1/enabled=0/g' /etc/default/apport
