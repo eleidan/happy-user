67fdebf1a8f7212dcfb34fbc53a20e110ec437a3
Oleg Kulyk <helh.saintman@gmail.com>
2022-04-16 22:28:21 +0300

    Update Docker section

--- install.sh
+++ install.sh
@@ -170,6 +170,36 @@ function prerequisites {
   return 1
 }
 
+################################################################################
+# Uninstall old versions
+# sudo apt-get remove docker docker-engine docker.io containerd runc
+
+# sudo apt-get install \
+#    ca-certificates \
+#    curl \
+#    gnupg \
+#    lsb-release
+
+# Add Docker’s official GPG key:
+# curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+# Set up the stable repository
+# echo \
+#  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+#  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+# Install
+# sudo apt-get update
+# sudo apt-get install docker-ce docker-ce-cli containerd.io
+
+# https://docs.docker.com/engine/install/linux-postinstall/
+# Manage Docker as a non-root users
+# sudo groupadd docker
+# sudo usermod -aG docker $USER
+# newgrp docker
+# https://github.com/eleidan/docker-bot/blob/master/src/bot
+
+
 echo "Docker" \
   && check_installed "docker" \
   && prerequisites \
@@ -178,24 +208,25 @@ echo "Docker" \
 
 
 ################################################################################
-# HTOP
+# sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+# sudo chmod +x /usr/local/bin/docker-compose
 echo "Docker Compose" \
   && check_installed "docker-compose" \
   && sudo pip install docker-compose \
   && echo $DONE
   # && sudo curl -L "https://github.com/docker/compose/releases/download/1.9.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose \
 
-
+# wget https://raw.githubusercontent.com/eleidan/docker-bot/master/src/bot
+# sudo mv bot /usr/local/bin/
+# sudo chmod +x /usr/local/bin/bot
 
 ################################################################################
-# HTOP
 echo "HTOP" \
   && check_installed "htop" \
   && sudo apt-get install -y htop \
   && echo $DONE
 
 ################################################################################
-# Tree
 echo "TREE" \
   && check_installed "tree" \
   && sudo apt-get install -y tree \
@@ -204,7 +235,6 @@ echo "TREE" \
 
 
 ################################################################################
-# Pygments
 echo "Pygments" \
   && check_installed "pygmentize" \
   && sudo apt install python-pygments \
